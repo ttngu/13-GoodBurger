97d72365d54a2b40ab7ccbd1e1bb0dba0cb5f274
ttngu <tammyn137@gmail.com>
2020-12-10 22:00:59 -0800

    added orm functions, may need updating

--- models/burger.js
+++ models/burger.js
@@ -2,6 +2,27 @@
 var orm = require("../config/orm.js");
 
 // Code that will call the ORM functions using specific input for ORM
+// ??? Can I change cb to something else ???
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    // Create Function
+    // These variables col and vals are arrays
+    create: function(cols, vals, cb) {
+        orm.create("burgers", col, vals, function(res) {
+            cb(res);
+        });
+    },
+    // Update Function
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
 
 // Export
 module.exports = burger;
