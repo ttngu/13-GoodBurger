b52271c8e71e60b0e5f18adb18500d4e92b8e5da
ttngu <tammyn137@gmail.com>
2020-12-11 01:40:12 -0800

    started routes, not finished

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,15 +5,25 @@ var router = express.Router();
 // Import burger.js model
 var burger = require("../models/burger.js");
 
-// Create GET route 
-
+// Create GET route
+router.get("/", function(req, res) {
+    // burger.all(function(data) {
+        
+    // }
+});
 
 
 // Create POST route 
+router.post("/api/burgers", function(req,res) {
 
+});
 
 
 // Create PUT route 
+router.put("/api/burgers/:id", function(req, res) {
+
+});
 
 
 // Export router
+module.exports = router;
