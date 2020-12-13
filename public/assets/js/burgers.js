1303156a659533782dcbf95ef8c51926206a5622
ttngu <tammyn137@gmail.com>
2020-12-12 20:37:39 -0800

    added pseudo code

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,11 @@
+// Wait to attach handlers until DOM is fully loaded
+
+// Create an onclick function to change devoured status
+// Send the PUT request
+// Page reload, for the updated list
+
+    
+// Create form on submit
+// On submit, prevent default
+// Send POST request
+// Page reload, for the updated list
