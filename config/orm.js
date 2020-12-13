05ce560de7aeb51053e2819c05a9342a138134e2
ttngu <tammyn137@gmail.com>
2020-12-12 19:48:58 -0800

    finished update function

--- config/orm.js
+++ config/orm.js
@@ -72,17 +72,21 @@ var orm = {
     update: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
 
-        
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
-
-}
-
-
-
-
-
-
-
+};
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
