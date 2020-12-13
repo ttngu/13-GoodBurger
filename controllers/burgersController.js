fc5fdc6e2fa2463a74b6c7064e552198498f93b2
ttngu <tammyn137@gmail.com>
2020-12-12 20:28:04 -0800

    updated put route

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -22,13 +22,28 @@ router.post("/api/burgers", function(req,res) {
         // Send back ID of new burger
         res.json({ id: result.insertId });
     });
-
 });
 
 
 // Create PUT route 
 router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
 
+    console.log("condition", condition);
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
 });
 
 
