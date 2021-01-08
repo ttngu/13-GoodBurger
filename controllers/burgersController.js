fce253e0cb3dc1bc1d999949c784575eedaa971d
ttngu <tammyn137@gmail.com>
2021-01-07 17:47:39 -0800

    added drop db

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 // Create POST route 
 router.post("/api/burgers", function(req,res) {
-    burger.create( ["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.create( ["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
         // Send back ID of new burger
         res.json({ id: result.insertId });
     });
