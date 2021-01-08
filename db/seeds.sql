01b63a1599dca8af060551c5a8aa7bae3734ae9b
ttngu <tammyn137@gmail.com>
2021-01-07 17:49:48 -0800

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUE ('OG Good Burger', false),
-INSERT INTO burgers (burger_name, devoured) VALUE ('Veggie Burger', true),
+INSERT INTO burgers (name, devoured) VALUES ('OG Good Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Good Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Good Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Yummy Good Burger', true);
